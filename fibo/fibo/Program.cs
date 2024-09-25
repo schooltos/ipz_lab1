df14f9c5738efcae3d6dca8c0e7b957b6c6cd34e
schooltos <maryan.kopchalyuk@gmail.com>
2024-09-26 01:07:08 +0300

    first commit

---
+++ fibo/fibo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the number of terms: ");
+            int terms = int.Parse(Console.ReadLine());
+
+            int firstNumber = 0, secondNumber = 1, nextNumber;
+
+            Console.WriteLine("Fibonacci sequence:");
+
+            for (int i = 1; i <= terms; i++)
+            {
+                if (i == 1)
+                {
+                    Console.Write(firstNumber + " ");
+                    continue;
+                }
+                if (i == 2)
+                {
+                    Console.Write(secondNumber + " ");
+                    continue;
+                }
+                nextNumber = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = nextNumber;
+
+                Console.Write(nextNumber + " ");
+            }
+        }
+    }
+}
